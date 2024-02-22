266325c87aa40ae5d0a8f5389912aafdd3c40861
p1ck234 <danil-vetrov47@mail.ru>
2024-02-22 12:54:46 +0400

    connected to api, took all posts out of api. started to make realization of likes

--- helpers.js
+++ helpers.js
@@ -13,3 +13,11 @@ export function getUserFromLocalStorage(user) {
 export function removeUserFromLocalStorage(user) {
   window.localStorage.removeItem("user");
 }
+
+export const sanitizeHtml = (htmlString) => {
+  return htmlString
+    .replaceAll("&", "&amp;")
+    .replaceAll("<", "&lt;")
+    .replaceAll(">", "&gt;")
+    .replaceAll('"', "&quot;");
+};
